3dd0b4b68b837ecff0690c69490aed9448795315
darkyiss7 <darkyiss7@gmail.com>
2022-05-09 21:39:04 +0200

    indent

--- LazherMeApp/CreateView.swift
+++ LazherMeApp/CreateView.swift
@@ -9,41 +9,46 @@ import SwiftUI
 
 struct CreateView: View {
     @State private var favoriteMode = "Team-Deathmatch"
-        var modes = ["Team-Deathmatch", "Free-For-All"]
+    var modes = ["Team-Deathmatch", "Free-For-All"]
     @State private var favoriteNombre = "2"
-        var nombres = ["2", "4", "6", "8", "10", "12"]
+    var nombres = ["2", "4", "6", "8", "10", "12"]
     @State private var favoriteTemps = "2"
-        var temps = ["10", "20", "30"]
+    var temps = ["10", "20", "30"]
     var body: some View {
         VStack{
             Text("Mode de jeu")
             Picker("Mode de jeu", selection: $favoriteMode) {
-                            ForEach(modes, id: \.self) {
-                                Text($0)
-                            }
-                        }
-                        .pickerStyle(.segmented)
+                ForEach(modes, id: \.self) {
+                    Text($0)
+                }
+            }
+            .pickerStyle(.segmented)
             //Text("Value: \(favoriteMode)")
             
             Text("Nombre de joueurs")
             Picker("Nombre de joueurs", selection: $favoriteNombre) {
-                            ForEach(nombres, id: \.self) {
-                                Text($0)
-                            }
-                        }
-                        .pickerStyle(.menu)
-           // Text("Value: \(favoriteNombre)")
+                ForEach(nombres, id: \.self) {
+                    Text($0)
+                }
+            }
+            .pickerStyle(.menu)
+            // Text("Value: \(favoriteNombre)")
             
             Text("Temps")
             Picker("Temps", selection: $favoriteTemps) {
-                            ForEach(temps, id: \.self) {
-                                Text($0)
-                            }
-                        }
+                ForEach(temps, id: \.self) {
+                    Text($0)
+                }
+            }
             .pickerStyle(.menu)
-           // Text("Value: \(favoriteTemps)")
+            // Text("Value: \(favoriteTemps)")
+            Button {
+                print("Image tapped!")
+            } label: {
+                CreerBouton()
+            }
         }
-        }
+    }
 }
 
 struct CreateView_Previews: PreviewProvider {
